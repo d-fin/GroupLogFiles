52d1b2ab0b2bca4d94305d0859bf30df4f110829
d-fin <dfinley5656@gmail.com>
2023-11-21 09:18:52 -0800

    Adding log file lookup functionality.

---
+++ GroupLogFiles/HandleFiles.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GroupLogFiles
+{
+    internal class HandleFiles
+    {
+        public HandleFiles() { }
+
+        public string PromptForLogFilePath()
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Text Files|*.txt|All Files|*.*";
+                openFileDialog.Title = "Select a Log File Location";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    return openFileDialog.FileName;
+                }
+            }
+            return null;
+        }
+
+        public string LocateLogFileDirectory()
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.Description = "Select a folder to store the log files:";
+                DialogResult result = folderBrowserDialog.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    string selectedFolderPath = folderBrowserDialog.SelectedPath;
+                    return selectedFolderPath;
+                }
+                else
+                {
+                    return "N/A";
+                }
+            }
+        }
+        public void UpdateLogPathInConfig(string logPath)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings["logPath"].Value = logPath;
+            config.Save(ConfigurationSaveMode.Modified);
+
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+
+        public bool DoesLogPathExist()
+        {
+            return File.Exists(GetLogFilePathFromConfig());
+        }
+        public string GetLogFilePathFromConfig()
+        {
+            return ConfigurationManager.AppSettings["logPath"];
+        }
+    }
+}
--- GroupLogFiles/Program.cs
+++ GroupLogFiles/Program.cs
@@ -2,14 +2,60 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GroupLogFiles
 {
     internal class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            Console.WriteLine("This application groups log files and sorts them by day.");
+            Console.WriteLine("Created by David Finley.\n\n");
+
+
+            bool cont;
+            HandleFiles fileHandler = new HandleFiles();
+            
+            if (fileHandler.DoesLogPathExist() == false)
+            {
+                Console.WriteLine("There is not a path to the log file.\nPress Enter to find log file....");
+                
+                if (ContinueOrExit() == true)
+                {
+                    string logFile = fileHandler.PromptForLogFilePath();
+                    Console.WriteLine($"The selected log file path : {logFile}\nPress Enter to continue....");
+                    if (ContinueOrExit() == true)
+                    {
+                        Console.Write("The log file saved in this location will be copied and saved to the log file directory.");
+                    }
+                    
+                }
+                else 
+                { 
+                    Environment.Exit(0); 
+                }
+            }
+        }
+
+        public static bool ContinueOrExit()
+        {
+            while (true)
+            {
+                var key = Console.ReadKey(intercept: true);
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    return true; 
+                }
+                else
+                {
+                    Console.WriteLine("Exiting Application....");
+                    Thread.Sleep(5000);
+                    return false;
+                }
+            }
         }
     }
 }
